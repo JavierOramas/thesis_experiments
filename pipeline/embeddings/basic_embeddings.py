a1f4e8b208dbee48a90c16cb8d0ea0f6a7fd48c7
JavierOramas <javiale2000@gmail.com>
2023-10-25 14:18:16 -0400

    topic model

--- pipeline/embeddings/basic_embeddings.py
+++ pipeline/embeddings/basic_embeddings.py
@@ -1,15 +1,13 @@
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
 import numpy as np
 import os
 
 class Embedding:
     def __init__(self, model_name="sentence-transformers/all-MiniLM-L6-v2"):
-        if model_name == "text-embedding-ada-002":
-            self.embedding_model = OpenAIEmbeddings(
-                model=model_name,
-                openai_api_base=os.environ["OPENAI_API_BASE"],
-                openai_api_key=os.environ["OPENAI_API_KEY"])
+        if model_name == "sentence-transformers/all-MiniLM-L6-v2":
+            self.embedding_model = SentenceTransformer(model_name)
         else:
             self.embedding_model = HuggingFaceEmbeddings(
                 model_name=model_name,
@@ -19,3 +17,6 @@ class Embedding:
     def encode(self, batch):
         embeddings = self.embedding_model.embed_documents(batch["text"])
         return {"text": batch["text"], "source": batch["source"], "embeddings": embeddings}
+
+    def get_llm(self):
+        return self.embedding_model

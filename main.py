affe4e36004fe886cd7ab4d56907406324cd2c28
JavierOramas <javiale2000@gmail.com>
2023-12-10 13:29:48 -0500

    generatinga some sort of text

---
+++ main.py
@@ -0,0 +1,11 @@
+from pipeline.doc_load.document_loader import llama_index_get_text as loader
+
+class MultiDocSummarizer():
+
+    def __init__(self):
+
+        pass
+
+    def load_documents(self, documents):
+
+        loader()
